6a657d0ee6434b899c03aaf8672325ef50d5808b
Leticia-Lakica <lakiacleticia@gmail.com>
2026-04-12 21:12:36 +0300

    adding drf_yasg swagger for debugging backend

--- backend/cinequest/urls.py
+++ backend/cinequest/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,4 +13,29 @@ urlpatterns = [
     path("api/users/", include("users.urls")),
     path("api/movies/", include("movies.urls")),
     path("api/recommendations/", include("recommendations.urls")),
+]
+
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="CineQuest API",
+        default_version='v1',
+        description="Movie Recommendation API",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # YOUR APIs
+    path('api/users/', include('users.urls')),
+    path('api/movies/', include('movies.urls')),
+    path('api/recommendations/', include('recommendations.urls')),
+
+    # SWAGGER
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0)),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0)),
 ]
