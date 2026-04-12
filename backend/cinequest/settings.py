af4d1086dbbeabeb89d2c60ea8f15f8920b27bbe
Leticia-Lakica <lakiacleticia@gmail.com>
2026-04-12 17:50:14 +0300

    Allow the backend server to run on device local host

--- backend/cinequest/settings.py
+++ backend/cinequest/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','localhost,127.0.0.1').split(',')
 
 INSTALLED_APPS = [
     "django.contrib.admin",
